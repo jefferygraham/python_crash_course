aebc0ba7c9d362908f1e1e1dd2cdf89e4f1d5db0
Jeff Graham <jg44387@vendor.bna.com>
2022-07-12 14:17:09 -0400

    add chapter 2 exercise(s)

---
+++ chapter_02/hello_world.py
@@ -0,0 +1,14 @@
+# message = "Hello Python world!"
+# print(message)
+
+# message = "Hello Python Crash Course World!"
+# print(message)
+
+# mesage = "Hello Python Crash Course Reader!"
+# print(mesage)
+
+simple_message = "This is a simple message."
+print(simple_message)
+
+simple_message = "This is another simple message."
+print(simple_message)
