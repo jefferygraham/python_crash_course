a36b5debea03e68ce6845ae3b607f3c40abd25c3
Jeff Graham <jg44387@vendor.bna.com>
2022-07-13 11:57:15 -0400

    change/add/remove elements

---
+++ chapter_03/cars.py
@@ -0,0 +1,2 @@
+cars = ["maserati", "mercedes-benz", "cadillac", "tesla"]
+print(f"I am going to own a {cars[1].title()}")
---
+++ chapter_03/names.py
@@ -0,0 +1,8 @@
+names = ['jeffery', 'brian', 'cordell']
+print(names[0].title())
+print(names[1].title())
+print(names[2].title())
+greeting = "Hello"
+print(f"{greeting} {names[0].title()}!")
+print(f"{greeting} {names[1].title()}!")
+print(f"{greeting} {names[2].title()}!")
