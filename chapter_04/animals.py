60314dde1452e9d2c4de0da56820242fc2c03570
Jeff Graham <jg44387@vendor.bna.com>
2022-07-15 10:20:06 -0400

    fix typo

--- chapter_04/animals.py
+++ chapter_04/animals.py
@@ -2,4 +2,4 @@ animals = ["snake", "turtle", "lizards" ]
 for animal in animals:
   print(f"A {animal} would make a great pet!")
 
-print("I'm going to them all!")
+print("I'm going to get them all!")
